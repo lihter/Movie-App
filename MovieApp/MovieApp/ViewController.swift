c25acc9afd8a93d3d6d83a826df75058b5fd359b
lihter <lihter.marko@gmail.com>
2021-07-12 15:42:54 +0200

    Initial commit

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Mac Use on 12.07.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
