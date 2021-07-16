e398ccf1b1b112b4c12ae393b3d0c4eec4b7d942
lihter <lihter.marko@gmail.com>
2021-07-16 08:25:42 +0200

    Added Alamofire through Swift Package Manager Adjusted movie model to API

--- MovieApp/MovieApp/Models/Movie.swift
+++ MovieApp/MovieApp/Models/Movie.swift
@@ -1,7 +1,34 @@
+struct MovieResponse: Decodable {
+    
+    private enum CodingKeys: String, CodingKey {
+        case movies = "results"
+    }
+    
+    let movies: [Movie]
+    
+}
+
 struct Movie: Decodable {
     
+    let identifier: Int
     let title: String
-    let description: String
-    let imagePath: String
+    let backdropPath: String?
+    let posterPath: String
+    let overview: String
+    let voteAverage: Double
+    let voteCount: Double
+    let releaseDate: String?
+    let genreIds: [Int]?
+    
+    private enum CodingKeys: String, CodingKey {
+        case identifier = "id"
+        case title, overview
+        case backdropPath = "backdrop_path"
+        case posterPath = "poster_path"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+        case releaseDate = "release_date"
+        case genreIds = "genre_ids"
+    }
     
 }
