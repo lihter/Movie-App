c632ffae09db4a2ed10694b8b1d601daa16a1adb
lihter <lihter.marko@gmail.com>
2021-07-22 14:03:43 +0200

    Movie Collection View

--- MovieApp/MovieApp/App/Features/MovieBrowser/MoviesViewController/HomePage2/HomePageTwoViewController.swift
+++ MovieApp/MovieApp/App/Features/MovieBrowser/MoviesViewController/HomePage2/HomePageTwoViewController.swift
@@ -7,11 +7,56 @@ class HomePageTwoViewController: UIViewController {
     var navigationView: MovieAppNavigationView!
     var searchBar: MovieSearchBar!
     var subcategoryView: SubcategoryView!
+    var moviesCollectionView: UICollectionView!
+    var flowLayout: UICollectionViewFlowLayout!
         
     override func viewDidLoad() {
         super.viewDidLoad()
         
         buildViews()
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        moviesCollectionView.register(NewMovieCell.self, forCellWithReuseIdentifier: NewMovieCell.reuseIdentifier)
+        moviesCollectionView.dataSource = self
+        moviesCollectionView.delegate = self
+    }
+    
+}
+
+extension HomePageTwoViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: NewMovieCell.reuseIdentifier,
+                for: indexPath) as? NewMovieCell
+        else {
+            return UICollectionViewCell()
+        }
+        let mockMovie = MovieViewModel(identifier: 13, title: "Mock", overview: "Bla bla bla", posterPath: URL(string: "https://image.tmdb.org/t/p/w185/qAZ0pzat24kLdO3o8ejmbLxyOac.jpg"))
+        
+        cell.populate(withMovie: mockMovie)
+        return cell
+    }
+    
+}
+
+extension HomePageTwoViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return NewMovieCell.cellSize
     }
     
 }
