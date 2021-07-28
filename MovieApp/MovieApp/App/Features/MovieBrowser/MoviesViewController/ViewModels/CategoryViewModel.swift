f0601c7449d776af8558351b61cc47e064c31f50
lihter <lihter.marko@gmail.com>
2021-07-28 15:31:39 +0200

    Removing comments

--- MovieApp/MovieApp/App/Features/MovieBrowser/MoviesViewController/ViewModels/CategoryViewModel.swift
+++ MovieApp/MovieApp/App/Features/MovieBrowser/MoviesViewController/ViewModels/CategoryViewModel.swift
@@ -1,8 +1,6 @@
 struct CategoryViewModel {
     
     let categoryKey: LocalCategory
-    /*let subcategories: [LocalSubcategory]
-    let movies: [MovieViewModel]*/
     let subcategoryMovies: [LocalSubcategory: [MovieViewModel]]
     
 }
