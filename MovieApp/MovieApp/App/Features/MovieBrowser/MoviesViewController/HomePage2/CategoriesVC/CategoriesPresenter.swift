04436b72b18f015f92b795251b7b565601c972fa
lihter <lihter.marko@gmail.com>
2021-08-16 12:40:33 +0200

    Missing files

---
+++ MovieApp/MovieApp/App/Features/MovieBrowser/MoviesViewController/HomePage2/CategoriesVC/CategoriesPresenter.swift
@@ -0,0 +1,89 @@
+final class CategoriesPresenter {
+    
+    private weak var delegate: CategoriesDelegate?
+    private let useCase: MoviesUseCaseProtocol!
+    private let router: AppRouter!
+        
+    init (useCase: MoviesUseCaseProtocol, router: AppRouter) {
+        self.useCase = useCase
+        self.router = router
+    }
+    
+    func setDelegate(delegate: CategoriesDelegate) {
+        self.delegate = delegate
+    }
+    
+    func getAllCategories(addToTableView: Bool = true) {
+        getPopularMovies(addToTableView: addToTableView)
+        getTrendingMovies(addToTableView: addToTableView)
+        getTopRatedMovies(addToTableView: addToTableView)
+    }
+    
+    func getMovies(for category: LocalCategory, genreId: Int) -> [MovieViewModel] {
+        let ucMovies = useCase.getMovies(for: category, genreId: genreId)
+        return ucMovies.map { MovieViewModel(fromModel: $0) }
+    }
+    
+    func getGenres(for category: LocalCategory) -> [Genre] {
+        if category == .trending {
+            return [.day, .week]
+        }
+        return [.action, .animation, .comedy, .scienceFiction, .thriller]
+    }
+    
+    func getPopularMovies(addToTableView: Bool = true) {
+        useCase.getPopularMovies { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(_):
+                if addToTableView {
+                    self.delegate?.addToTableView(category: .popular)
+                }
+            case .failure(let error):
+                print("Loading error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func getTrendingMovies(addToTableView: Bool = true) {
+        useCase.getTrendingMovies { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(_):
+                if addToTableView {
+                    self.delegate?.addToTableView(category: .trending)
+                }
+            case .failure(let error):
+                print("Loading error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func getTopRatedMovies(addToTableView: Bool = true) {
+        useCase.getTopRatedMovies { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(_):
+                if addToTableView {
+                    self.delegate?.addToTableView(category: .topRated)
+                }
+            case .failure(let error):
+                print("Loading error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func selectedMovie(withId movieId: Int) {
+        router.showDetailScreen(for: movieId)
+    }
+    
+    func toggleFavorite(_ movieId: Int) {
+        useCase.toggleFavorite(movieId)
+        
+        delegate?.reloadData()
+    }
+    
+}
