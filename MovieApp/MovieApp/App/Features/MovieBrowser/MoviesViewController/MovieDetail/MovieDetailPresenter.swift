dc55d573a04aba8cf356f9ff57398cacd0054743
lihter <lihter.marko@gmail.com>
2021-08-17 12:18:33 +0200

    Fix from older PR

--- MovieApp/MovieApp/App/Features/MovieBrowser/MoviesViewController/MovieDetail/MovieDetailPresenter.swift
+++ MovieApp/MovieApp/App/Features/MovieBrowser/MoviesViewController/MovieDetail/MovieDetailPresenter.swift
@@ -9,6 +9,10 @@ final class MovieDetailPresenter {
     
     let movieId: Int!
     
+    var isFavorite: Bool {
+        useCase.checkIfFavorite(for: movieId)
+    }
+    
     init (useCase: MoviesUseCaseProtocol, router: AppRouter, for movieId: Int) {
         self.useCase = useCase
         self.router = router
@@ -98,10 +102,6 @@ final class MovieDetailPresenter {
         useCase.toggleFavorite(movieId)
         
         delegate?.reloadData()
-    }
-    
-    func checkIfFavorite() -> Bool {
-        useCase.checkIfFavorite(for: movieId)
     }
     
 }
